4912083e343fc966a62cbb83d3178edacd09d2bb
KaushikSarveswaran <Kaushik3497@yahoo.co.in>
2019-08-17 22:16:53 +0530

    bash script for valid

--- utils.py
+++ utils.py
@@ -82,9 +82,9 @@ def create_validation_arg_parser():
     parser = argparse.ArgumentParser(description = 'train setup for segmentation')
     parser.add_argument('--model_type',type=str,help='select model type: unet,dcan,dmtn,psinet,convmcd')      
     parser.add_argument('--val_path',type=str,help='path to img jpg files')
-    parser.add_argument('--model_file',type=str,default=None,help='model_file')
-    parser.add_argument('--cuda_no',type=int,default=0,help='cuda number')
+    parser.add_argument('--model_file',type=str,help='model_file')
     parser.add_argument('--save_path',type=str,help='results save path.')
+    parser.add_argument('--cuda_no',type=int,default=0,help='cuda number')
     
     return parser
 
---
+++ valid.sh
@@ -0,0 +1,5 @@
+val_path=''
+save_path=''
+model_type=''
+model_file=''
+sudo /home/htic/anaconda2/envs/torch4/bin/python valid.py --val_path ${val_path} --save_path ${save_path} --model_type ${model_type} --model_file ${model_file} 
