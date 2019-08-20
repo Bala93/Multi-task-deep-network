70b27aa1789d13d70d7112d5f4961216441fdd1d
Balamurali <balamuralim.1993@gmail.com>
2019-08-20 22:14:39 +0530

    Update valid.sh

--- valid.sh
+++ valid.sh
@@ -2,4 +2,4 @@ val_path=''
 save_path=''
 model_type=''
 model_file=''
-sudo /home/htic/anaconda2/envs/torch4/bin/python valid.py --val_path ${val_path} --save_path ${save_path} --model_type ${model_type} --model_file ${model_file} 
+python valid.py --val_path ${val_path} --save_path ${save_path} --model_type ${model_type} --model_file ${model_file} 
